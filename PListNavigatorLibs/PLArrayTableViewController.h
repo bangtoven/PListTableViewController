ec3a49f8383ddcc4eec3b4082d5852621f6e2b20
Jungho Bang <me@bangtoven.com>
2013-07-30 17:05:32 +0900

    first commit

---
+++ PListNavigatorLibs/PLArrayTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  PLArrayTableViewController.h
+//  PListNavigator
+//
+//  Created by Xeron on 13. 7. 30..
+//  Copyright (c) 2013년 Bangtoven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PLArrayTableViewController : UITableViewController
+
+@end
---
+++ PListNavigatorLibs/PLNavigationController.m
@@ -0,0 +1,38 @@
+//
+//  PLNavigationController.m
+//  PListNavigator
+//
+//  Created by Xeron on 13. 7. 30..
+//  Copyright (c) 2013년 Bangtoven. All rights reserved.
+//
+
+#import "PLNavigationController.h"
+
+@interface PLNavigationController ()
+
+@end
+
+@implementation PLNavigationController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
